fd317f0e0c7e43bb4dafda41a43845a1faba205b
Christian Tiquis <christian.tiquis>
2024-02-25 13:36:28 +0800

    added routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Popular from './components/Popular';
-import { useGlobalContext } from './context/global';
 
 function App() {
-  const global = useGlobalContext();
-  console.log(global);
   return (
-    <Popular />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Popular />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
