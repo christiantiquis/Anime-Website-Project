d727efd2b13a67dd547478ae98eecd1d84ee1764
Christian Tiquis <christian.tiquis>
2024-02-25 14:26:55 +0800

    added animeitem

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Popular from './components/Popular';
+import AnimeItem from './components/AnimeItem';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Popular />} />
+        <Route path="/anime/:id" element={<AnimeItem />} />
       </Routes>
     </BrowserRouter>
   );
